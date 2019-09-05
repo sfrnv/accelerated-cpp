8e8591e705a9e8d10fe7dd2da821e45a2a44e8ac
Roman Safronov <sofronov32@gmail.com>
2019-09-05 22:03:00 +0300

    First simple c++ program

---
+++ hello_world/src/hello_world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, world!" << std::endl;
+	std::cout << "#include <iostream>" << std::endl << "int main() {" << std::endl << "\t"
+	<< "std::cout << \"Hello, world!\" << std::endl;" << std::endl << "\t" << "return 0;"
+	<< std::endl << "}" << std::endl;
+	return 0;
+}
