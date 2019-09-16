0b975438c3b39e75a92a4e57400b71951aab6ad0
Roman Safronov <sofronov32@gmail.com>
2019-09-16 21:47:16 +0300

    Quartiles exerise was added

--- student_grades/src/student_grades.cpp
+++ student_grades/src/student_grades.cpp
@@ -16,10 +16,13 @@ using std::vector;
 
 int median();
 int average();
+void quartiles();
 
 int main() {
 	// return average();
-	return median();
+	// return median();
+	quartiles();
+	return 0;
 }
 
 int average() {
@@ -110,4 +113,51 @@ int median() {
 		<< setprecision(prec) << endl;
 
 	return 0;
+}
+
+void quartiles() {
+	cout << "Enter all your numbers, followed by end-of-file: ";
+
+	vector<double> set;
+	double x;
+
+	while (cin >> x)
+		set.push_back(x);
+
+	typedef vector<double>::size_type vec_sz;
+	vec_sz size = set.size();
+
+	if (size == 0) {
+		cout << endl << "You must enter your numbers.  Please try again." << endl;
+		return;
+	}
+
+	sort(set.begin(), set.end());
+	double q1, q2, q3;
+	vec_sz mid = size / 2;
+	vec_sz quad;
+	if (size % 2 == 0) { // ok
+		q2 = (set[mid] + set[mid - 1] ) / 2;
+		quad = mid / 2;
+		if (mid % 2 == 0) {
+			q1 = (set[quad] + set[quad - 1]) / 2;
+			q3 = (set[mid + quad] + set[mid + quad - 1]) / 2;
+		} else {
+			q1 = set[quad];
+			q3 = set[mid + quad];
+		}
+	} else {
+		q2 = set[mid];
+		quad = (mid - 1) / 2;
+		if (mid % 2 == 0) {
+			cout << "mid = " << mid << "; quad = " << quad << endl;
+			q1 = (set[quad] + set[quad + 1]) / 2;
+			q3 = (set[(mid + 1) + quad] + set[(mid + 1) + (quad + 1)]) / 2;
+		} else {
+			q1 = set[quad];
+			q3 = set[mid + quad + 1];
+		}
+	}
+	streamsize prec = cout.precision();
+	cout << setprecision(3) << q1 << " " << q2 << " " << q3 << setprecision(prec) << endl;
 }
