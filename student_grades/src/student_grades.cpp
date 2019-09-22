0b456e1e3bd0bc173d9b92e8a1e0b45f8a360a6f
Roman Safronov <sofronov32@gmail.com>
2019-09-22 23:10:40 +0300

    Erasing failing students in place

--- student_grades/src/student_grades.cpp
+++ student_grades/src/student_grades.cpp
@@ -60,19 +60,19 @@ bool fgrade(const Student_info& s) {
 	return grade(s) < 60;
 }
 
-// separate passing and failing student records: first try
+// second try: correct but potentially slow
 vector<Student_info> extract_fails(vector<Student_info>& students) {
+	vector<Student_info> fail;
+	vector<Student_info>::size_type i = 0;
 
-	vector<Student_info> pass, fail;
-
-	for (vector<Student_info>::size_type i = 0; i != students.size(); ++i) {
-		if (fgrade(students[i]))
+	// invariant:elements [0, i) of students represent passing grades
+	while (i != students.size()) {
+		if (fgrade(students[i])) {
 			fail.push_back(students[i]);
-		else
-			pass.push_back(students[i]);
+			students.erase(students.begin() + i);
+		} else
+			++i;
 	}
-
-	students = pass;
 	
 	return fail;
 }
